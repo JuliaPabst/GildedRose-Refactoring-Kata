9c697b47a7d3e6df2bf431740053a3b37c24a241
Jules Pabst <julia.pabst@kununu.com>
2025-06-17 08:25:51 +0200

    test: sulfuras does not decrease in quality or sellIn

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -50,4 +50,14 @@ class GildedRoseTest {
         assertEquals(50, items[0].quality);
     }
 
+    @Test
+    void sulfuras_doesNotChange() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
 }
